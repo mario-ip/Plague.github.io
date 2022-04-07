f8de8fef9ab20c9c49fb9098feb7284cd154e878
mario-ip <101064921+mario-ip@users.noreply.github.com>
2022-04-07 14:28:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+        document.getElementsByTagName('button')[0].addEventListener('click', function(){
+                document.body.className='blanco';
+        });
+        document.getElementsByTagName('button')[2].addEventListener('click', function(){
+                document.body.className='multicolor';
+        });
+
+        document.getElementsByTagName('button')[1].addEventListener('click',function() {
+                document.body.className='negro';
+        })
